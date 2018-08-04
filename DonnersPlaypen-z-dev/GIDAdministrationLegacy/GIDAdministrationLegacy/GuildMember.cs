14503694affc7746ded50f88877f4d945f32b796
Zachary Lewis <donnergid@hotmail.com>
2018-08-03 22:18:06 -0700

    Adding in a solution built with .NET instead of the XAML based UWP.

---
+++ DonnersPlaypen-z-dev/GIDAdministrationLegacy/GIDAdministrationLegacy/GuildMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIDAdministrationLegacy
+{
+    class GuildMember
+    {
+
+        GuildMember()
+        {
+            Guild tmpGuild = new Guild();
+            currentGuild = tmpGuild; 
+        }
+
+        string charName;
+        string charRace;
+        string charClass;
+        string charSpec;
+        string locale; // Eventually this will be replaced with a Locale obj pulling from Wow APIs. 
+        Guild currentGuild; 
+        
+        // Need to add in a obj that defines talants. 
+    }
+
+    class Guild
+    {
+        string guildName;
+        string realmName; 
+    }
+}
