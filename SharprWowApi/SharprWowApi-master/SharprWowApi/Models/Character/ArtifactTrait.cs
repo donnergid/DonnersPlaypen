e1836dfe6993b92b208fe0dacaa481c8f0088948
Zachary Lewis <donnergid@hotmail.com>
2018-08-08 12:51:51 -0700

    Further cleaning of the branch.

--- SharprWowApi/SharprWowApi-master/SharprWowApi/Models/Character/ArtifactTrait.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharprWowApi.Models.Character
-{
-    public class ArtifactTrait
-    {
-        public int Id { get; set; }
-        public int Rank { get; set; }
-    }
-}
--- SharprWowApi/SharprWowApi-master/SharprWowApi/Models/Character/CharacterItemAppearance.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SharprWowApi.Models.Character
-{
-    public class CharacterItemAppearance
-    {
-        public int ItemAppearanceModId { get; set; }
-    }
-}
